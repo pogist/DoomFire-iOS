1bb8eee8acff12685d78429415e9d706f348b476
pogist <murilo.paixao.2@gmail.com>
2019-01-31 19:52:25 -0200

    spritekit: setup pixel nodes inside main scene

--- DoomFire/MainScene.swift
+++ DoomFire/MainScene.swift
@@ -10,11 +10,30 @@ import SpriteKit
 
 class MainScene: SKScene {
   var doomFire: DoomFire!
+  var nodes: [SKShapeNode]!
+
+  let nodeSize = 7
 
   override func didMove(to view: SKView) {
     super.didMove(to: view)
 
     doomFire = DoomFire(width: 10, height: 10)
-    print(doomFire.pixels)
+
+    nodes = doomFire.pixels.map { pixel in
+      let node = self.makeNode(fromPixel: pixel)
+      self.addChild(node)
+
+      return node
+    }
+  }
+
+  private func makeNode(fromPixel pixel: DoomFire.Pixel) -> SKShapeNode {
+    let node = SKShapeNode(rectOf: CGSize(width: nodeSize, height: nodeSize))
+    node.strokeColor = .clear
+    node.fillColor = DoomPixelColor.pixelColor(forIntensity: pixel.intensity)
+    node.position = CGPoint(
+      x: Int(pixel.position.x) * nodeSize + (nodeSize / 2),
+      y: Int(pixel.position.y) * nodeSize + (nodeSize / 2))
+    return node
   }
 }
