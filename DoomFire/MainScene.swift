88fd63be53cfe622d3e76f012de21e437f6589f2
pogist <murilo.paixao.2@gmail.com>
2019-01-31 19:04:02 -0200

    spritekit: initial setup

---
+++ DoomFire/MainScene.swift
@@ -0,0 +1,13 @@
+//
+//  MainScene.swift
+//  DoomFire
+//
+//  Created by Murilo Paixão on 31/01/19.
+//  Copyright © 2019 Murilo Paixão. All rights reserved.
+//
+
+import SpriteKit
+
+class MainScene: SKScene {
+  
+}
--- DoomFire/ViewController.swift
+++ DoomFire/ViewController.swift
@@ -7,14 +7,22 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    let skView = SKView(frame: view.bounds)
+    skView.showsFPS = true
+    skView.showsNodeCount = true
+    skView.isAsynchronous = true
+    skView.preferredFramesPerSecond = 60
+
+    view.addSubview(skView)
+
+    let scene = MainScene(size: skView.frame.size)
+    skView.presentScene(scene)
   }
-
-
 }
-
