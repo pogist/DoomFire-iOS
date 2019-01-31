7d6540c61e9b6a5bc575d4810f0e9eaa8dd13b17
pogist <murilo.paixao.2@gmail.com>
2019-01-31 12:01:11 -0200

    Initial Commit

---
+++ DoomFire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DoomFire
+//
+//  Created by Murilo Paixão on 31/01/19.
+//  Copyright © 2019 Murilo Paixão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
